71523f7114edc7e283661b3ce47da197496e46dc
Julio Romero <jromeroacive@gmail.com>
2024-03-27 10:56:40 -0430

    Index controller api

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+      $notes = Note::all();
+      return response()->json($notes, 200);
     }
 
     /**
